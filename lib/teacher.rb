cfcc7656a69299522b90651fc767477d4b4acbaa
David Fuentes <davidf0827@gmail.com>
2020-08-15 17:52:44 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -4,9 +4,6 @@ class Teacher < User
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
   def teach 
-  string = KNOWLEDGE.sample(1)
-  puts string
-  binding.pry 
-  true
+    KNOWLEDGE.sample
   end
 end
